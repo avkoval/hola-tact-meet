ca6a4806a621cd8a43b64d4454b9bc2dd3f43009
Oleksii Koval <alex@koval.kharkov.ua>
2025-07-05 00:13:17 +0300

    Fake login finished, separate schema, permissions middleware
    WIP on change theme and manage users
    
    .

--- src/ok/hola_tact_meet/core.clj
+++ src/ok/hola_tact_meet/core.clj
@@ -11,264 +11,52 @@
    [ring.middleware.proxy-headers :refer [wrap-forwarded-remote-addr]]
    [ring.middleware.session.cookie :refer [cookie-store]]
    [ring.middleware.cookies :refer [wrap-cookies]]
-   [selmer.parser :refer [render-file] :as selmer]
-   [starfederation.datastar.clojure.api :as d*]
-   [starfederation.datastar.clojure.adapter.ring :refer [->sse-response on-open]]
+   [selmer.parser :as selmer]
+   [ok.hola_tact_meet.middleware :as middleware]
    [ok.hola_tact_meet.utils :as utils]
-   [ok.hola_tact_meet.db :as db]
-   [aero.core :refer [read-config]]
+   [ok.hola_tact_meet.views :as views]
    [clojure.java.io]
-   [clojure.pprint :refer [pprint]]
-   [ok.oauth2.utils :refer [get-oauth-config]]
    [ok.session.utils :refer [encode-secret-key]]
-   [ring.middleware.oauth2 :refer [wrap-oauth2]]
    [clojure.tools.logging :as log]
-   [faker.generate :as gen]
-   [datomic.client.api :as d]
    )
   (:gen-class))
 
-(defn app-config []
-  (read-config (clojure.java.io/resource "config.edn")))
-
-(defn localhost?
-  "Check if the given address is a localhost address.
-   Handles IPv4, IPv6, and hostname variants."
-  [addr]
-  (when addr
-    (or (= addr "127.0.0.1")
-        (= addr "::1")
-        (= addr "0:0:0:0:0:0:0:1")
-        (= addr "[0:0:0:0:0:0:0:1]")
-        (= addr "localhost"))))
-
-(defn home [{session :session :as request}]
-  (let [oauth2-config (get-oauth-config (app-config))
-        remote-addr (:remote-addr request)
-        dev_mode (localhost? remote-addr)
-        ]
-    
-    (log/info "Home page accessed from" remote-addr "dev_mode: " dev_mode)
-    (if (get-in session [:userinfo :logged-in])
-      (response/redirect "/app")
-      {:status 200
-       :headers {"Content-Type" "text/html"}
-       :body (render-file "templates/home.html" {:google_client_id (get-in oauth2-config [:google :client-id])
-                                                 :google_launch_uri (get-in oauth2-config [:google :launch-uri])
-                                                 :google_login_uri "/google-login"
-                                                 :host (get-in request [:headers "host"])
-                                                 :dev_mode dev_mode})})))
-
-(defn app-main [{session :session}]
-  (log/info "Main app page accessed")
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (render-file "templates/main.html" {:userinfo (:userinfo session)})})
-
-(defn admin-manage-users [{session :session}]
-  (let [users (db/get-all-users)
-        userinfo (:userinfo session)]
-    (log/info "admin-manage-users accessed")
-    {:status 200
-     :headers {"Content-Type" "text/html"}
-     :body (render-file "templates/users.html" {:users users :userinfo userinfo})}))
-
-(defn admin-update-user-access-level [request]
-  (->sse-response 
-   request
-   {on-open
-    (fn [sse]
-      ;; (pprint request)
-      (let [user_id (get-in request [:query-params "user_id"])
-            params (utils/datastar-query request)
-            new_access_level (get params (keyword (str "accessLevel" user_id)))]
-        (println params)
-        (println new_access_level)
-        (when (and user_id new_access_level)
-          (d/transact (db/get-conn) {:tx-data [{:db/id (Long/parseLong user_id)
-                                                :user/access-level new_access_level}]}))
-        (d*/with-open-sse sse
-          (d*/merge-fragment! sse (render-file "templates/users_list.html" {:users (db/get-all-users)})))))}))
-
-(defn test-session [{session :session}]
-  (let [count   (:count session 0)
-        session (assoc session :count (inc count))]
-    (-> (response/response (str "You accessed this page " count " times."))
-        (assoc :session session))))
-
-(defn google-login [{session :session :as request}]
-  (let [login-count   (:login-count session 0)
-        session (assoc session :count (inc login-count))]
-    (log/info "Google login attempt from" (:remote-addr request))
-    (-> (response/response (str "Logged IN. " login-count " times."))
-        (assoc :session session))))
-
-
-(defn login-user-by-id 
-  "Log in user by their database ID"
-  [{session :session} user-id userinfo]
-
-  (let [updated-session (assoc session :userinfo (assoc userinfo 
-                                                        :logged-in true
-                                                        :user-id user-id))]
-    (db/update-last-login! user-id)
-    (log/info "User logged in:" (:email userinfo) "ID:" user-id)
-    (-> (response/redirect "/app")
-        (assoc :session updated-session))))
-
-(defn login 
-  "Main login function that creates user and logs them in"
-  [request userinfo]
-  (let [user-id (db/create-user userinfo)]
-    (log/info "User created:" (:email userinfo) "ID:" user-id)
-    (login-user-by-id request user-id userinfo)))
-
-(defn fake-login-page [request]
-  (let [users (db/get-all-users)]
-    (log/info "Fake login page accessed from" (:remote-addr request))
-    {:status 200
-     :headers {"Content-Type" "text/html"}
-     :body (render-file "templates/fake.html" {:users users})}))
-
-(defn fake-login-existing [request]
-  (let [user-id-str (get-in request [:params "user-id"])
-        user-id (Long/parseLong user-id-str)
-        user-data (db/get-user-by-id user-id)
-        userinfo {:name (:user/name user-data)
-                  :email (:user/email user-data)
-                  :family-name (:user/family-name user-data)
-                  :given-name (:user/given-name user-data)
-                  :picture (:user/picture user-data)
-                  :auth-provider "fake"
-                  :access-level (:user/access-level user-data)}]
-    (log/info "Fake login as existing user:" (:email userinfo))
-    (login-user-by-id request user-id userinfo)))
-
-(defn fake-login-new [request]
-  (let [params (:params request)
-        fake-userinfo {:name (get params "name")
-                       :email (get params "email")
-                       :family-name (get params "family-name")
-                       :given-name (get params "given-name")
-                       :picture ""
-                       :auth-provider "fake"
-                       :access-level (get params "access-level")}]
-    (log/info "Creating new fake user:" (:email fake-userinfo))
-    (login request fake-userinfo)))
-
-
-(defn fake-user-data []
-  {:userinfo 
-   {:name (gen/word)
-    :email (utils/gen-email)
-    :family-name (gen/word)
-    :given-name (gen/word)
-    :picture ""
-    :auth-provider "fake"
-    :access-level (rand-nth ["admin" "user" "staff"])}}
-  )
-
-
-(defn fake-generate-random-data [request]
-  (->sse-response request
-                  {on-open
-                   (fn [sse]
-                     (d*/with-open-sse sse
-                       (d*/merge-fragment! sse
-                        (render-file "templates/fake-user-form.html" (fake-user-data)))
-                       ))}))
-
-
-(defn logout [_]
-  (-> (response/redirect "/")
-      (assoc :session {})))
-
-
-(defn wrap-request-logging [handler]
-  (fn [request]
-    (if (utils/sse-endpoint? request)
-      ;; For SSE, just log the request and pass through
-      (do
-        (log/info (format "%s %s (SSE)"
-                          (:request-method request)
-                          (:uri request)))
-        (handler request))
-      ;; For regular requests, do full logging
-      (let [start-time (System/currentTimeMillis)
-            response (handler request)
-            duration (- (System/currentTimeMillis) start-time)]
-        (log/info (format "%s %s %d (%dms)"
-                          (:request-method request)
-                          (:uri request)
-                          (:status response)
-                          duration))
-        response))))
-
-(defn wrap-require-auth [handler]
-  (fn [request]
-    (if (get-in request [:session :userinfo :logged-in])
-      (handler request)
-      (response/redirect "/"))))
-
-(defn wrap-localhost-only [handler]
-  (fn [request]
-    (let [remote-addr (:remote-addr request)]
-      (if (localhost? remote-addr)
-        (handler request)
-        {:status 403
-         :headers {"Content-Type" "text/plain"}
-         :body "Access denied. This endpoint is only available from localhost."}))))
-
-(defn wrap-force-https [handler]
-  (fn [request]
-    (if (utils/sse-endpoint? request)
-      ;; Skip HTTPS processing for SSE to avoid interfering with streaming
-      (handler request)
-      (let [proto (get-in request [:headers "x-forwarded-proto"])
-            request' (if (= proto "https")
-                       (assoc request :scheme :https)
-                       request)]
-        (handler request')))))
-
-(defn my-wrap-oauth2 [base-handler]
-  (let [config (get-oauth-config (app-config))]
-    (wrap-oauth2 base-handler config)))
 
 
 (def base-app
   (ring/ring-handler
    (ring/router
     [
-     ["/" {:get home}]
+     ["/" {:get views/home}]
      ["/favicon.ico" {:get (fn [_] (response/file-response "resources/public/favicon.ico"))}]
      ["/assets/*" (ring/create-resource-handler)]
-     ["/app" {:get (wrap-require-auth app-main)}]
-     ["/test-session" {:get test-session}]
-     ["/google-login" {:post google-login}]
-     ["/login/fake" {:get (wrap-localhost-only fake-login-page)}]
-     ["/login/fake/existing" {:post (wrap-localhost-only fake-login-existing)}]
-     ["/login/fake/new" {:post (wrap-localhost-only fake-login-new)}]
-     ["/login/fake/generate-random-data" {:get fake-generate-random-data}]
-     ["/admin/manage-users" {:get admin-manage-users}]
-     ["/admin/manage-users/update-user-access-level" {:get admin-update-user-access-level}]
-     ["/logout" {:get logout}]
+     ["/app" {:get (middleware/wrap-require-auth views/app-main)}]
+     ["/test-session" {:get views/test-session}]
+     ["/google-login" {:post views/google-login}]
+     ["/login/fake" {:get (middleware/wrap-localhost-only views/fake-login-page)}]
+     ["/login/fake/existing" {:post (middleware/wrap-localhost-only views/fake-login-existing)}]
+     ["/login/fake/new" {:post (middleware/wrap-localhost-only views/fake-login-new)}]
+     ["/login/fake/generate-random-data" {:get views/fake-generate-random-data}]
+     ["/admin/manage-users" {:get (middleware/wrap-require-admin views/admin-manage-users)}]
+     ["/admin/manage-users/update-user-access-level" {:get (middleware/wrap-require-admin views/admin-update-user-access-level)}]
+     ["/logout" {:get views/logout}]
      ])
    (constantly {:status 404, :body "Not Found."})))
 
 
 (defn create-app []
-  (let [secret-key (encode-secret-key (:secret-key (app-config)))]
+  (let [secret-key (encode-secret-key (:secret-key (utils/app-config)))]
     (-> base-app
         utils/wrap-json-params
         wrap-params
-        my-wrap-oauth2
+        middleware/my-wrap-oauth2
         (wrap-session {:store (cookie-store {:key secret-key})
                        :cookie-attrs {:http-only true}
                        })
-        wrap-request-logging
+        middleware/wrap-request-logging
         wrap-cookies
         wrap-forwarded-remote-addr
-        wrap-force-https
+        middleware/wrap-force-https
         )))
 
 (def app (create-app))
---
+++ src/ok/hola_tact_meet/middleware.clj
@@ -0,0 +1,69 @@
+(ns ok.hola-tact-meet.middleware
+  (:require
+   [ring.util.response :as response]
+   [ok.hola_tact_meet.utils :as utils]
+   [clojure.java.io]
+   [ok.oauth2.utils :refer [get-oauth-config]]
+   [ring.middleware.oauth2 :refer [wrap-oauth2]]
+   [clojure.tools.logging :as log]
+   )
+  (:gen-class))
+
+(defn wrap-request-logging [handler]
+  (fn [request]
+    (if (utils/sse-endpoint? request)
+      ;; For SSE, just log the request and pass through
+      (do
+        (log/info (format "%s %s (SSE)"
+                          (:request-method request)
+                          (:uri request)))
+        (handler request))
+      ;; For regular requests, do full logging
+      (let [start-time (System/currentTimeMillis)
+            response (handler request)
+            duration (- (System/currentTimeMillis) start-time)]
+        (log/info (format "%s %s %d (%dms)"
+                          (:request-method request)
+                          (:uri request)
+                          (:status response)
+                          duration))
+        response))))
+
+(defn wrap-require-auth [handler]
+  (fn [request]
+    (if (get-in request [:session :userinfo :logged-in])
+      (handler request)
+      (response/redirect "/"))))
+
+(defn wrap-localhost-only [handler]
+  (fn [request]
+    (let [remote-addr (:remote-addr request)]
+      (if (utils/localhost? remote-addr)
+        (handler request)
+        {:status 403
+         :headers {"Content-Type" "text/plain"}
+         :body "Access denied. This endpoint is only available from localhost."}))))
+
+(defn wrap-require-admin [handler]
+  (fn [request]
+    (let [access-level (get-in request [:session :userinfo :access-level])]
+      (if (= access-level "admin")
+        (handler request)
+        {:status 403
+         :headers {"Content-Type" "text/plain"}
+         :body "Access denied. Admin privileges required."}))))
+
+(defn wrap-force-https [handler]
+  (fn [request]
+    (if (utils/sse-endpoint? request)
+      ;; Skip HTTPS processing for SSE to avoid interfering with streaming
+      (handler request)
+      (let [proto (get-in request [:headers "x-forwarded-proto"])
+            request' (if (= proto "https")
+                       (assoc request :scheme :https)
+                       request)]
+        (handler request')))))
+
+(defn my-wrap-oauth2 [base-handler]
+  (let [config (get-oauth-config (utils/app-config))]
+    (wrap-oauth2 base-handler config)))
